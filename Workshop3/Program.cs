fe1c2dbd2f444d0a13579934b750be56f7b9672a
atemengue <moafembe@gmail.com>
2024-07-03 19:26:24 +0100

    getting started exercices workshop 4

---
+++ Workshop3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Worshop 4: Array
+
+string[] studentNames = new string[] { "Regis", "Brayan", "Farida", "Priscile" };
+
+string[] matricules = new string[4];
+
+matricules[0] = "12U0434";
+matricules[1] = "25U0931";
+matricules[2] = "19B1234";
+matricules[3] = "22T0621";
+
+int[] idStudents = new int[4];
+
+// remplissage des index.
+for (int index = 0; index < idStudents.Length; index++)
+{
+    idStudents[index] = index;
+}
+
+// affichage des noms
+for (int index = 0; index < studentNames.Length; index++)
+{
+    Console.WriteLine(studentNames[index]);
+}
